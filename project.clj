8220823eae937f46e7dafba20a016b33814734b4
MayDaniel <yamnad1@gmail.com>
2010-08-25 15:53:26 +0100

    project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject clj-chat "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject clj-chat "0.1.0"
+  :description "A simple chat server."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [clj-store "0.3.0"]
                  [clj-time "0.1.0-RC1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
- 
